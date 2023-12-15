34e874f936950c4dfbf100d764565eecf30cf72c
2minchul <2_minchul@naver.com>
2023-12-15 14:29:05 +0900

    WIP

--- constants/constant.go
+++ constants/constant.go
@@ -5,7 +5,7 @@ import "time"
 const (
 	ReadPinNumber  = 18 // Change this to your GPIO PIN number
 	WritePinNumber = 17
-	Delay          = 500 * time.Millisecond
+	Delay          = 100 * time.Millisecond
 	StartPattern   = 0b10000110
 	StartDelay1    = 90 * time.Millisecond
 	StartDelay2    = 45000 * time.Microsecond
