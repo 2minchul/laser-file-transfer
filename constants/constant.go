09a48b02182c3d5e40886364efc09044570ec648
2minchul <2_minchul@naver.com>
2023-12-15 14:17:09 +0900

    WIP

--- constants/constant.go
+++ constants/constant.go
@@ -6,7 +6,7 @@ const (
 	ReadPinNumber  = 18 // Change this to your GPIO PIN number
 	WritePinNumber = 17
 	Delay          = 1000 * time.Millisecond
-	StartPattern   = 0b01000101
+	StartPattern   = 0b10000110
 	StartDelay1    = 9 * time.Millisecond
 	StartDelay2    = 4500 * time.Microsecond
 )
