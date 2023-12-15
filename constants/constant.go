61d76086556bb9575d5ae802e6db3e33639946a4
2minchul <2_minchul@naver.com>
2023-12-15 14:50:10 +0900

    WIP

--- constants/constant.go
+++ constants/constant.go
@@ -8,5 +8,5 @@ const (
 	Delay          = 50 * time.Millisecond
 	StartPattern   = 0b10000110
 	StartDelay1    = 90 * time.Millisecond
-	StartDelay2    = 30 * time.Millisecond
+	StartDelay2    = 45 * time.Millisecond
 )
